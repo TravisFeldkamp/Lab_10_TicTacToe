4ae163e64bf5454fdc6a69c87cf2dadd587990b3
Travis Feldkamp <feldkatr@mail.uc.edu>
2023-03-28 23:04:25 -0400

    Initial commit

---
+++ src/TicTacToe.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    private static final int ROW =3;
+    private static final int COL =3;
+    private static String board [][]=new String[ROW][COL];
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+    }
+}
+
